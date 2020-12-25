18845735cca216ca9f8234cca8f3ef71e0597624
Mohammed Mudassir Ali <alimudassir12@gmail.com>
2020-12-25 18:23:20 +0530

    Implement Search and Delete Tasks

--- app.js
+++ app.js
@@ -8,23 +8,60 @@ const filter = document.querySelector('#filter');
 //Load all event listeners
 loadEventListeners();
 
-function loadEventListeners () {
+function loadEventListeners() {
   //Add Task event
   form.addEventListener('submit', addTask);
+  //Remove Task
+  taskList.addEventListener('click', removeTask);
+  //Clear All Tasks
+  clearBtn.addEventListener('click', clearTasks);
+  //Filter tasks
+  filter.addEventListener('keyup', filterTasks);
 };
 
 function addTask(event) {
-  if(taskInput.value === ''){
+  if (taskInput.value === '') {
     alert('Task cannot be empty!');
+  } else {
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    li.appendChild(document.createTextNode(taskInput.value));
+    const link = document.createElement('a');
+    link.className = 'delete-item secondary-content';
+    link.innerHTML = '<i class = "fa fa-remove"></i>';
+    li.appendChild(link);
+    taskList.appendChild(li);
+    taskInput.value = '';
+    event.preventDefault();
   }
-  const li = document.createElement('li');
-  li.className = 'collection-item';
-  li.appendChild(document.createTextNode(taskInput.value));
-  const link = document.createElement('a');
-  link.className = 'delete-item secondary-content';
-  link.innerHTML = '<i class = "fa fa-remove"></i>';
-  li.appendChild(link);
-  taskList.appendChild(li);
-  taskInput.value = '';
-  event.preventDefault();
 };
+
+function removeTask(event) {
+  if (event.target.parentElement.classList.contains('delete-item')) {
+    if (confirm('Are you sure?')) {
+      event.target.parentElement.parentElement.remove();
+    }
+  }
+};
+
+function clearTasks(event) {
+  if (confirm('Are you sure you want to clear all tasks?')) {
+    while (taskList.firstChild) {
+      taskList.removeChild(taskList.firstChild);
+    }
+  }
+};
+
+function filterTasks(event) {
+  let text = event.target.value.toLowerCase();
+  document.querySelectorAll('.collection-item').forEach(
+    (task) => {
+      const currentText = task.firstChild.textContent;
+      if (currentText.toLowerCase().indexOf(text) != -1) {
+        task.style.display = 'block';
+      } else {
+        task.style.display = 'none';
+      }
+    }
+  );
+}
